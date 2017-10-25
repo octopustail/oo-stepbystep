a1d0b08c44ab8e865b7f1134a37b291575c24471
Seaborn Lee <seabornlee.cn@gmail.com>
2017-10-25 14:30:51 +0800

    Refine practice 1

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,7 +1,7 @@
-function Person(name, age){
+function Person(name, age) {
     this.name = name;
     this.age = age;
-    this.introduce = function(){
+    this.introduce = function() {
         return `My name is ${this.name}. I am ${this.age} years old.`;
     };
 }
