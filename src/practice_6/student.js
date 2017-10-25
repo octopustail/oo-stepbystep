37f121a8a951fb6337d65123a9eb73e81c514c7b
Seaborn Lee <seabornlee.cn@gmail.com>
2017-10-25 11:20:16 +0800

    Fix test

--- src/practice_6/student.js
+++ src/practice_6/student.js
@@ -6,7 +6,7 @@ export default class Student extends Person{
         this.klass = klass;
     }
     introduce(){
-        return super.introduce() + " I am a Student. I am at Class" + this.klass +".";
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
     }
 }
 
