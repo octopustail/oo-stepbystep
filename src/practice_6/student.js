9823097f42571506391e76ed943597774d27ca77
Seaborn Lee <seabornlee.cn@gmail.com>
2017-10-25 11:19:22 +0800

    Fix test

--- src/practice_6/student.js
+++ src/practice_6/student.js
@@ -3,7 +3,7 @@ import Person from "./person";
 export default class Student extends Person{
     constructor(name, age, klass){
         super(name, age);
-        this.klass = this.klass;
+        this.klass = klass;
     }
     introduce(){
         return super.introduce() + " I am a Student. I am at Class" + this.klass +".";
