1f1a0a12868654aa2e0ffe90402e83bf55fac64e
Seaborn Lee <seabornlee.cn@gmail.com>
2017-10-25 14:32:49 +0800

    Refine practice 2

--- spec/practice_2/main_spec.js
+++ spec/practice_2/main_spec.js
@@ -1,10 +1,9 @@
 "use strict";
-import _ from "lodash";
 import chai from "chai";
-import sinon from "sinon";
 import sinonChai from "sinon-chai";
 const expect = chai.expect;
 chai.use(sinonChai);
+
 import Person from "../../src/practice_2/person.js";
 
 describe("Person", () => {
@@ -12,15 +11,11 @@ describe("Person", () => {
         const person = new Person("Tom", 21);
         expect(person.name).to.equal("Tom");
         expect(person.age).to.equal(21);
-
     });
 
     it("should have a method introduce, introduce person with name and age", () => {
         const person = new Person("Tom", 21);
-
         const introduce = person.introduce();
-
         expect(introduce).to.equal("My name is Tom. I am 21 years old.");
-
     });
 });
--- src/practice_2/person.js
+++ src/practice_2/person.js
@@ -1,9 +1,9 @@
-function Person(name, age){
+function Person(name, age) {
     this.name = name;
     this.age = age;
 }
 
-Person.prototype.introduce = function(){
+Person.prototype.introduce = function() {
     return `My name is ${this.name}. I am ${this.age} years old.`;
 };
 
